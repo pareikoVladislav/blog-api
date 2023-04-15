9b85ecd45a249e4c522733421fca1c1ca619d28f
Vladislav Pareiko <parieiko.vlad@gmail.com>
2023-04-15 22:13:22 +0400

    Add new  app

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&m02$0cy9nx^0t_u&x0=$j9g$p#b8tc6)7if-*2s551+g09kn1'
+SECRET_KEY = os.environ.get("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
