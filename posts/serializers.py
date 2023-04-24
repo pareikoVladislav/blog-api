29563ca5d7aef990392a3d3de31862ff6e01f759
Vladislav Pareiko <parieiko.vlad@gmail.com>
2023-04-24 20:23:22 +0400

    Add new serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Post
@@ -14,3 +15,9 @@ class PostSerializer(serializers.ModelSerializer):
         )
         model = Post
 
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username", ]
