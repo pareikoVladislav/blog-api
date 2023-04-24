d4f99ff32533863567970185c999108ba4a45d4a
Vladislav Pareiko <parieiko.vlad@gmail.com>
2023-04-24 20:23:43 +0400

    Add new view

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-    path("", PostList.as_view(), name="post_list"),
-]
+from .views import PostViewSet, UserViewSet
+
+router = SimpleRouter()
+
+router.register("users", PostViewSet, basename="users"),
+router.register("", UserViewSet, basename="posts"),
+
+urlpatterns = router.urls
